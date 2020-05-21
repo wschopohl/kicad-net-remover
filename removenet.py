e63756b70098477c5241770ab1340bc647a612ee
Wieland Schopohl <info@medien-produkt.de>
2020-05-21 19:08:25 +0200

    better match of Net Name

--- removenet.py
+++ removenet.py
@@ -1,14 +1,15 @@
 import sys
 
 net_found = False
+search_name = "(name {})".format(sys.argv[3])
 
 with open(sys.argv[1]) as search:
     with open(sys.argv[2],'w') as output:
         for line in search:
             tmpline = line.lstrip()  # remove '\n' at end of line
-            if tmpline.startswith("(net") and sys.argv[3] in tmpline:
+            if tmpline.startswith("(net") and search_name in tmpline:
                 net_found = True
-            if tmpline.startswith("(net") and not sys.argv[3] in tmpline:
+            if tmpline.startswith("(net") and not search_name in tmpline:
                 net_found = False
             if not net_found:
                 output.write(line)
